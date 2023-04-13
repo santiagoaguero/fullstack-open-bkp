a59891b8d80134881717cc6ac993cbd432f2ec83
Leonidas <hotdog979@hotmail.com>
2023-04-12 20:35:15 -0400

    delete unnecesary files

--- part1/anecdotes/src/main.jsx
+++ part1/anecdotes/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+
+const App = () => <h1>hi</h1>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
